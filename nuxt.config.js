199da4e0ce808a341f8028ac03a2abf0087fd378
illmat <mail@pekone.de>
2020-03-09 19:29:18 +0100

    cleanup

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Antje Theobalt - Diplom Restauratorin - Steinkonservierung',
     meta: [{
         charset: 'utf-8'
       },
